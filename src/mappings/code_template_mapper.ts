73564b466d76508c318f4475d7f8d4d8fc5a6c95
Khalid Rinaldy <khalid.rinaldy@nusantarainfrastructure.com>
2023-08-05 07:21:14 +0700

    fix mapper

--- src/mappings/code_template_mapper.ts
+++ src/mappings/code_template_mapper.ts
@@ -5,37 +5,37 @@ let mappings: any = {};
 mappings['assets'] = t.assets;
 mappings['enum'] = t.enumConst;
 mappings['typedef'] = t.typeDef;
-mappings['appButton'] = t.appButton;
+mappings['app_button'] = t.appButton;
 mappings['extension'] = t.extensionConst;
 mappings['helper'] = t.helper;
 mappings['toast'] = t.toast;
-mappings['appRootScaffold'] = t.appRootScaffold;
+mappings['app_root_scaffold'] = t.appRootScaffold;
 mappings['color'] = t.color;
 mappings['theme'] = t.theme;
 
 //---- Mapping Services
 mappings['timezone'] = t.timezone;
-mappings['loggingInterceptor'] = t.loggingInterceptor;
-mappings['tokenInterceptor'] = t.tokenInterceptor;
-mappings['baseClient'] = t.baseClient;
+mappings['logging_interceptor'] = t.loggingInterceptor;
+mappings['token_interceptor'] = t.tokenInterceptor;
+mappings['base_client'] = t.baseClient;
 mappings['logger'] = t.logger;
 mappings['network'] = t.network;
-mappings['responsePagination'] = t.responsePagination;
+mappings['response_pagination'] = t.responsePagination;
 mappings['response'] = t.response;
 mappings['url'] = t.url;
-mappings['authStorage'] = t.authStorage;
+mappings['auth_storage'] = t.authStorage;
 mappings['directory'] = t.directory;
-mappings['registerAdapter'] = t.registerAdapter;
-mappings['storageKey'] = t.storageKey;
+mappings['register_adapter'] = t.registerAdapter;
+mappings['storage_key'] = t.storageKey;
 mappings['storage'] = t.storage;
 
 //---- Mapping Features
-mappings['loginBinding'] = t.loginBinding;
-mappings['loginController'] = t.loginController;
-mappings['requestLogin'] = t.requestLogin;
-mappings['authRepository'] = t.authRepository;
-mappings['someWidgets'] = t.someWidgets;
-mappings['loginView'] = t.loginView;
+mappings['login_binding'] = t.loginBinding;
+mappings['login_controller'] = t.loginController;
+mappings['request_login'] = t.requestLogin;
+mappings['auth_repository'] = t.authRepository;
+mappings['some_widgets'] = t.someWidgets;
+mappings['login_view'] = t.loginView;
 
 mappings['routes'] = t.routes;
 
