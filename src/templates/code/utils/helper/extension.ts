df7e6845b4116a282e762351337014e0bce808d7
Khalid Rinaldy <khalid.rinaldy@nusantarainfrastructure.com>
2023-08-07 07:50:52 +0700

    add dateformatter extension

--- src/templates/code/utils/helper/extension.ts
+++ src/templates/code/utils/helper/extension.ts
@@ -1,11 +1,19 @@
 export const extensionConst = `
 part of 'helper.dart';
+
 extension NumberFormatter on int {
-    String get moneyFormat {
-      var moneyFormatter = NumberFormat("###,###,000");
-      if (toInt() == 0) {
-        return "0";
-      }
-      return moneyFormatter.format(toInt());
+  String get moneyFormat {
+    var moneyFormatter = NumberFormat("###,###,000");
+    if (toInt() == 0) {
+      return "0";
     }
-  }`;
+    return moneyFormatter.format(toInt());
+  }
+}
+
+extension DateFormatter on DateTime {
+  String get toRegularDateFormat => DateFormat('MMMM dd, yyyy').format(this);
+  String get toRegularDateAndTimeFormat => DateFormat('MMMM dd, yyyy HH:mm:s').format(this);
+  String get getTime => DateFormat('HH:mm:s').format(this);
+}
+`;
