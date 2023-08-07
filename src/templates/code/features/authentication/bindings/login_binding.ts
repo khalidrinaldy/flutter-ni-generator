1ca5e7fc5a0564259a85d2b07087724f3b86e02b
Khalid Rinaldy <khalid.rinaldy@nusantarainfrastructure.com>
2023-08-07 08:36:01 +0700

    fix template login

--- src/templates/code/features/authentication/bindings/login_binding.ts
+++ src/templates/code/features/authentication/bindings/login_binding.ts
@@ -4,6 +4,6 @@ import 'package:get/get.dart';
 class LoginBinding implements Bindings {
     @override
     void dependencies() {
-      Get.lazyPut<LoginController>(() => LoginController(AuthRepository()));
+      Get.lazyPut<LoginController>(() => LoginController(AuthenticationRepository()));
     }
   }`;
--- src/templates/code/features/authentication/views/login_view.ts
+++ src/templates/code/features/authentication/views/login_view.ts
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginView extends GetView<LoginController> {
-    const LoginView({Key? key}) : super(key: key);
-  
-    @override
-    Widget build(BuildContext context) {
-      return AppRootWrap(
-        child: Scaffold();
-    }
-  }`;
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppRootWrap(
+      child: Scaffold(),
+    );
+  }
+}
+`;
